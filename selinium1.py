78369bfdf66849e40ebf6537e6b7bdc48eb040ca
kajithirive <58061701+kajithirive@users.noreply.github.com>
2020-08-22 14:10:46 +0530

    Update selinium1.py

--- selinium1.py
+++ selinium1.py
@@ -9,7 +9,8 @@ from selenium.webdriver.common.keys import Keys
 # The database will be created in the location where 'py' file is saved
 conn = sqlite3.connect('loan_data.db')  
 c = conn.cursor() 
-driver = webdriver.Chrome(r'C:\Python38\chromedriver.exe')
+#driver = webdriver.Chrome(r'C:\Python38\chromedriver.exe')
+driver = webdriver.Chrome(r'C:\chromedriver_win32\chromedriver.exe')
 #browser = webdriver.Firefox()
 driver.get("http://localhost:5000/application.html")
 name=driver.find_element_by_name("name")
